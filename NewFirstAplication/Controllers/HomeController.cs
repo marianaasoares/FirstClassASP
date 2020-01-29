8691062d86997b5fa42a0eba422af32f9f323329
marianaasoares <mariana.dsantos@al.infnet.edu.br>
2020-01-29 09:21:29 -0300

    Add project files.

---
+++ NewFirstAplication/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NewFirstAplication.Models;
+
+namespace NewFirstAplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string OnGet(
+            string arg)
+        {
+
+            return $"OnGet result: {arg}";
+        }
+
+        [HttpPost]
+        public string OnPost(
+            string arg)
+        {
+
+            return $"OnPost result: {arg}";
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
